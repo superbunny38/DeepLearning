9646827816c685959226ed3485d4a8cbdf999e1f
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2023-01-03 22:57:33 +0900

    Add files via upload

---
+++ SupplementaryMaterials/BinaryDiceCoeff.py
@@ -0,0 +1,25 @@
+def diceCoeff(pred, gt, smooth=1e-5, activation='sigmoid'):
+    r""" computational formula：
+        dice = (2 * (pred ∩ gt)) / (pred ∪ gt)
+    """
+
+    if activation is None or activation == "none":
+        activation_fn = lambda x: x
+    elif activation == "sigmoid":
+        activation_fn = nn.Sigmoid()
+    elif activation == "softmax2d":
+        activation_fn = nn.Softmax2d()
+    else:
+        raise NotImplementedError("Activation implemented for sigmoid and softmax2d activation function operation")
+
+    pred = activation_fn(pred)
+
+    N = gt.size(0)
+    pred_flat = pred.view(N, -1)
+    gt_flat = gt.view(N, -1)
+
+    intersection = (pred_flat * gt_flat).sum(1)
+    unionset = pred_flat.sum(1) + gt_flat.sum(1)
+    loss = (2 * intersection + smooth) / (unionset + smooth)
+
+    return 1.-loss.sum() /N
