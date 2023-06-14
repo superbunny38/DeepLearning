e95ec5032628706f5e109e75beeac1611a41ac7c
Chaeeun Ryu <48243487+superbunny38@users.noreply.github.com>
2023-06-14 15:57:31 +0900

    Update softmax_from_scratch.py

--- pytorch/softmax_from_scratch.py
+++ pytorch/softmax_from_scratch.py
@@ -1,7 +1,10 @@
 import torch
-def softmax(net_outputs):
-  denom = torch.sum(torch.exp(net_outputs)).item()
-  return net_outputs/denom
+def softmax(net_outputs,num_class = 10):
+  denom = torch.sum(torch.exp(net_outputs),dim = 1)
+  result = torch.zeros(net_outputs.size())
+  for idx in range(num_class):
+    result[:,idx] = torch.div(net_outputs[:,idx],torch.sum(torch.exp(net_outputs),dim = 1))
+  return result
 
 net_output = model(images))
 softmax_out = softmax(net_output)
